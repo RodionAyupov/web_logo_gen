3510b9d2c0e0c4d5b77ed6a4a19257d8719ad9a3
RodionAyupov <ayrodion@gmail.com>
2021-05-05 15:45:21 +0300

    Added canvas tricks

---
+++ mysite/static/js/logo_gen.js
@@ -0,0 +1,31 @@
+
+function LogoGenerator(text){
+//    alert(text);
+//    var text = document.getElementsByName("input_text").value;
+    var canvas = document.getElementById("myCanvas");
+    canvas.width = window.innerWidth; //document.width is obsolete
+    canvas.height = window.innerHeight*0.9; //document.height is obsolete
+    var canvasWidth = canvas.width;
+    var canvasHeight = canvas.height;
+    var ctx = canvas.getContext("2d");
+    var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    function drawCircle() {
+        ctx.fillStyle = "white";
+        ctx.beginPath();
+        ctx.arc(Math.random()*600, Math.random()*600, 10, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+    for (var i = 1; i < 99999; i++)
+        window.clearInterval(i);
+
+    window.setInterval(function(){
+      drawCircle();
+    }, 500 / 1); // 25 times per second
+
+    drawCircle();
+}
+
