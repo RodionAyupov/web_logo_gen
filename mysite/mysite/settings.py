40e004131073613c82acc630067a90d21f7a4637
RodionAyupov <ayrodion@gmail.com>
2021-05-05 15:58:33 +0300

    Added canvas tricks

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/rodionayupov/web_logo_gen/mysite/static/'
+STATIC_ROOT = '/home/rodionayupov/web_logo_gen/mysite/static'
 
 STATICFILES_DIRS = (
   os.path.join(BASE_DIR, "static",),
